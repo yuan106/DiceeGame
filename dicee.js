fdb395b2c9da255db42a0755914c09f6d9b81df0
yuan106 <61606121+yuan106@users.noreply.github.com>
2020-06-09 18:46:36 -0700

    challenge1

---
+++ dicee.js
@@ -0,0 +1,14 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomDiceImage = "images/dice" + randomNumber1 + ".png"; //images/dice1.png-6
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+var randomDiceImage = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage);
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw!";
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins! ";
+} else {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩 ";
+}
